43240f980500a4a39499844fe47aff00f993a138
vm0303 <vmadhav33@gmail.com>
2025-11-28 10:53:01 -0500

    Spinnner.jsx will be rewmoved as this file has been renamed to Loading Screen.jsx

--- src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Spinner() {
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-white/40 dark:bg-black/40 backdrop-blur-md z-50">
-            <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin" />
-        </div>
-    );
-}
