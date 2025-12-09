23336051d8847ff01ad0a2e3a2d8ecc9029652ad
vm0303 <vmadhav33@gmail.com>
2025-12-09 10:08:16 -0500

    Made more changes -Changed the click event to tap -Adding some autoclose for a few toasts -added css in index.css for the recents dropdown and Suggestions.jsx dropdown - Change the dark mode for Sunny to make it look more presentable.

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-export default function Suggestion({item, onSelect}) {
+export default function Suggestion({ item, onSelect }) {
     return (
         <button
+            onPointerDown={(e) => {
+                e.preventDefault();   // ← prevents blur on trackpad
+                e.stopPropagation();  // ← prevents outside click
+            }}
             onClick={onSelect}
             className="text-left px-4 py-2 text-slate-700 dark:text-slate-200 hover:bg-slate-100 dark:hover:bg-slate-700 w-full"
         >
@@ -10,3 +14,4 @@ export default function Suggestion({item, onSelect}) {
         </button>
     );
 }
+
