6f45ef16b3368faad8d6f2332b50aa0a1d85b913
vm0303 <vmadhav33@gmail.com>
2025-12-27 14:05:10 -0500

    In the process of making the app responsive for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer(theme) {
     return (
         <footer className="py-6 text-center">
-            <p className={`text-lg ${theme.text}`}>
+            <p className={`text-lg ${theme.text} min-[240px]:text-sm`}>
                 Created by Vishal Madhav</p>
         </footer>
     );
